4fe9e98104d2c712f1b0b6d6bbd1bdd5a5ed9fad
Karol3Navarro <karo.navarro@duocuc.cl>
2025-04-27 21:38:54 -0400

    Modificacion de Producto

--- index/urls.py
+++ index/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('home', views.home, name='home'),
     path('login', views.logout, name='login'),
     path('registro', views.registro, name='registro'),
-    path('producto_add', views.producto_add, name='producto_add'),
     path('producto/<int:id_producto>/', views.ver_producto, name='ver_producto'),
     path('perfil/', views.perfil_usuario, name='perfil_usuario'),
     path('mapa/', views.map, name='mapa'),
