a712c432bb468ff31b4fa022f00f0700c24712a9
Belen <bel.ruz@duocuc.cl>
2025-06-16 19:48:23 -0400

    .

--- Dm/admin.py
+++ Dm/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Canal, CanalUsuario, CanalMensaje, ConfirmacionEntrega
 # Register your models here.
 
+# Definición de las clases inline para los modelos CanalMensaje y CanalUsuario
+# Estas clases permiten editar los mensajes y usuarios asociados a un canal 
+# directamente desde la interfaz de administración
 class CanalMensajeInline(admin.TabularInline):
     model =CanalMensaje
     extra=1
@@ -9,13 +12,16 @@ class CanalMensajeInline(admin.TabularInline):
 class CanalUsuarioInline(admin.TabularInline):
     model =CanalUsuario
     extra=1
-
+    
+# Clase CanalAdmin que define cómo se verá el modelo Canal en el panel de administración de Django
+# Utiliza las clases inline para mostrar los mensajes y usuarios asociados al canal
 class CanalAdmin(admin.ModelAdmin):
     inlines =[CanalMensajeInline, CanalUsuarioInline]
 
     class Meta:
         model =Canal
 
+#registra los modelos en el admin de Django
 admin.site.register(Canal, CanalAdmin)
 admin.site.register(CanalUsuario)
 admin.site.register(CanalMensaje)
--- Dm/forms.py
+++ Dm/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 from .models import ConfirmacionEntrega
 
+# Formulario para enviar mensajes en un canal
+# Utiliza CharField con un widget Textarea para permitir la entrada de texto en varias líneas
 class FormMensajes(forms.Form):
     mensaje = forms.CharField(widget=forms.Textarea(attrs={
         "class": "formulario_ms",
         "placeholder": "Escribe tu mensaje"
     }))
 
+# Formulario para confirmar la entrega de un pedido
+# Utiliza ModelForm para generar automáticamente los campos basados en el modelo ConfirmacionEntrega
 class ConfirmacionEntregaForm(forms.ModelForm):
     class Meta:
         model = ConfirmacionEntrega
